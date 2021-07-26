534d06b3ddd772c9e29740b5c3c5e011aeaa5bba
Decagon <decagon@Decagons-MacBook-Air.local>
2021-07-26 21:49:52 +0100

    My first Golang Task

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	. "fmt"
+)
+
+func main () {
+	var name string = "Clinton Adebayo"
+	var year int = 2021
+	stack := "Golang"
+	squad := "SQ008"
+
+	Printf("My Name is %s, I am %s developer a member of squad %s of year %d\n", name, stack, squad, year)
+}
