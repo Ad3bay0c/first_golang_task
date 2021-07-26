2e37d9090e858322305cc8e9b79c2e4dffcaa41f
Decagon <decagon@Decagons-MacBook-Air.local>
2021-07-26 21:54:39 +0100

    Changed short assignment to Long Assignment

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 func main () {
 	var name string = "Clinton Adebayo"
 	var year int = 2021
-	stack := "Golang"
-	squad := "SQ008"
+	var stack string = "Golang"
+	var squad string = "SQ008"
 
 	Printf("My Name is %s, I am %s developer a member of squad %s of year %d\n", name, stack, squad, year)
 }
